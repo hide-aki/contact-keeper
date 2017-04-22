be2f0f108cd91e8a4487cb8f1da1b5bdcabd66f6
jeffreyfriedman <jeff@eurodollaryen.com>
2017-04-22 12:14:31 -0400

    complete_crud

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
+
   def show
     @user = User.find(params[:id])
       render json: {
@@ -13,9 +13,9 @@ class Api::V1::UsersController < ApplicationController
         phone_number: @user.phone_number
       }, status: :ok
   end
-  
+
+  # curl -X POST -H "Content-Type: application/json" -d '{ "user": { "username": "jfriedman", "first_name": "Jeff", "last_name": "Friedman", "email": "test@test.com", "phone_number": "617-123-4567", "password": "password", "password_confirmation": "password" }}' http://localhost:3000/api/v1/users
   def create
-    binding.pry
     @user = User.new(user_params)
     if @user.save
       render json: {
@@ -26,14 +26,30 @@ class Api::V1::UsersController < ApplicationController
         phone_number: @user.phone_number
       }, status: :ok
     else
-      render json: { error: @user.errors.full_messages.join(', ') }
+      render json: { error: @user.errors.full_messages.join(', ') }, status: :bad_request
     end
   end
-  
+
+  # curl -X PUT -H "Content-Type: application/json" -d '{ "user": { "phone_number": "617-111-2222" }}' http://localhost:3000/api/v1/users/1
+  def update
+    @user = User.find(params[:id])
+    @user.assign_attributes(user_params)
+    if @user.save
+      render json: {
+        message: "Update successful."
+      }, status: :ok
+    else
+      render json: { error: @user.errors.full_messages.join(', ') }, status: :bad_request
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+  end
+
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :username, :email, :phone_number, :password, :password_confirmation)
   end
 end
-
-# curl -X POST -H "Content-Type: application/json" -d '{ "user": {"username": "jfriedman", "first_name": "Jeff", "last_name": "Friedman", "email": "test@test.com", "phone_number": "617-123-4567", "password": "password", "password_confirmation": "password" }}' http://localhost:8080/api/v1/users
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
+  validates :password, presence: true, on: :create
+  validates :password_confirmation, presence: true, on: :create
   validates :username, presence: true, uniqueness: true
 end
