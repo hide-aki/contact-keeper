f8cf2f12ac346753996aea4313e366939282b78f
Jeffrey Friedman <jeff@eurodollaryen.com>
2017-05-30 23:58:02 +0000

    Eager load lib directory

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module ContactKeeper
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths << "#{Rails.root}/lib"
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
